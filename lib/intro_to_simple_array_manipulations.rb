f840f15f6686151704430d96b51e1021b935dd9f
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-23 17:55:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,7 +12,6 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
 end
 
 def using_pop(continents)
-  array = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  array = continents.pop
+ continents.pop
 end
 
